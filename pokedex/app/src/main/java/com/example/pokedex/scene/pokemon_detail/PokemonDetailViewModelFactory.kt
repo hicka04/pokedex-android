242f74ed53fe6e52dad2247d0cdae50d619ce1bb
hicka04 <hicka04@gmail.com>
2022-05-03 15:39:39 +0900

    Update Libraries

--- pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_detail/PokemonDetailViewModelFactory.kt
+++ pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_detail/PokemonDetailViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class PokemonDetailViewModelFactory(private val pokemonName: String): ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PokemonDetailViewModel::class.java)) {
             return PokemonDetailViewModel(pokemonName) as T
         }
